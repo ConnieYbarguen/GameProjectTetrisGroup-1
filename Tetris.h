50adbb6eaa8acb5e3563c85f11d228b8a930596c
José <chaqquererj@gmail.com>
2022-12-19 18:32:50 -0500

    creando metodos publicosen tetris.h

--- Tetris.h
+++ Tetris.h
@@ -6,7 +6,33 @@
 class Tetris
 {
 public:
-	
+	Tetris()
+	{
+	}
+	~Tetris()
+	{
+	}
+
+	void setCurrentTime(Uint32 t)
+	{
+		currentTime = t;
+	}
+
+	bool isrunning()
+	{
+		return running;
+	}
+
+	bool isvalid();
+
+	bool init(const char* title);
+	void nextTetrimino();
+	void handleEvents();
+	void setRectPos(SDL_Rect& rect, int x = 0, int y = 0, int w = BlockW, int h = BlockH);
+	void moveRectPos(SDL_Rect& rect, int x, int y);
+	void gameplay();
+	void updateRender();
+	void clean();
 private:
 	
 };
