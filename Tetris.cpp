b1a3e9ee75c3de1388e7e61c786a977efad9bc59
José <chaqquererj@gmail.com>
2022-12-19 18:59:04 -0500

    definiendo metodo  de validez en tetris.cpp

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 #include "Tetris.h"
+bool Tetris::isvalid()
+{
+	for (int i = 0; i < 4; i++)
+		if (items[i].x < 0 || items[i].x >= Cols || items[i].y >= Lines)
+			return false;
+		else if (field[items[i].y][items[i].x])
+			return false;
+	return true;
+}
