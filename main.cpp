3332eb80e835ba070f10b764bb01545ec4abcd6b
oscarreivi <molinaoscar159753@gmail.com>
2022-12-19 10:57:15 -0500

    agregando codigo

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
 #include "Tetris.h"
 #include <iostream>
+int main(int argc, char* argv[])
+{
+	using namespace std;
+	srand(time(0));
+	Tetris* tetris = new Tetris();
+	const char* title = "Tetris";
+	if (tetris->init(title))
+	{
+		
+	}
+	else
+	{
+		cout << "Failed to initialize";
+	}
+
+	tetris->clean();
+
+	return 0;
+}
 
