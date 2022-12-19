8c9074fca8526ab5d318f208d11cc6362524af11
José <chaqquererj@gmail.com>
2022-12-19 18:47:13 -0500

    creandovariable privadas en tetris.h

--- Tetris.h
+++ Tetris.h
@@ -34,6 +34,26 @@ public:
 	void updateRender();
 	void clean();
 private:
+	enum { ScreenW = 600, ScreenH = 760 };
+	enum { BlockW = 36, BlockH = 36 };
+	enum { Lines = 20, Cols = 10 };
+	SDL_Window* window = NULL;
+	SDL_Renderer* render = NULL;
+	SDL_Texture* background = NULL, * blocks = NULL;
+	SDL_Rect srcR = { 0, 0, BlockW, BlockH }, destR = { 0, 0, BlockW, BlockH };
+
+	bool running = false;
+	int field[Lines][Cols] = { 0 };
+	static const int figures[7][4];
+	struct Point
+	{
+		int x, y;
+	} items[4], backup[4];
+	int color = 1;
+	int dx = 0;
+	bool rotate = false;
+	unsigned int delay = 300;
+	Uint32 startTime = 0, currentTime = 0;
 	
 };
 
