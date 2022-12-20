b49b8320db34743685d8f915a20e411e3bd33045
José <chaqquererj@gmail.com>
2022-12-19 19:25:21 -0500

    definiendo método actualizar de render(updateteRender)

--- Tetris.cpp
+++ Tetris.cpp
@@ -55,3 +55,27 @@ void Tetris::handleEvents()
 	if (state[SDL_SCANCODE_DOWN])
 		delay = 50;
 }
+
+void Tetris::updateRender()
+{
+	SDL_RenderCopy(render, background, NULL, NULL);
+	for (int i = 0; i < Lines; i++)
+		for (int j = 0; j < Cols; j++)
+			if (field[i][j])
+			{
+				setRectPos(srcR, field[i][j] * BlockW);
+				setRectPos(destR, j * BlockW, i * BlockH);
+				moveRectPos(destR, BlockW, ScreenH - (Lines + 1) * BlockH);
+				SDL_RenderCopy(render, blocks, &srcR, &destR);
+			}
+	for (int i = 0; i < 4; i++)
+	{
+		setRectPos(srcR, color * BlockW);
+		setRectPos(destR, items[i].x * BlockW, items[i].y * BlockH);
+		moveRectPos(destR, BlockW, ScreenH - (Lines + 1) * BlockH);
+		SDL_RenderCopy(render, blocks, &srcR, &destR);
+	}
+
+	SDL_RenderPresent(render);
+
+}
